662d2695af4444ba27ff37a5ddc95c0fe763a31a
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-07-06 14:58:40 -0400

    commit tree 7069813bf4daf92b9c2b4ed396501373984f55c0

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import { addItem } from  './actions/items';
 
 class App extends Component {
 
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
+  handleOnClick = (event) => {
+    this.props.addItem();
   }
 
   render() {
+    debugger;
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
+        <button onClick={this.handleOnClick}>
           Click
           </button>
         <p>{this.props.items.length}</p>
@@ -21,10 +22,24 @@ class App extends Component {
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.items
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     items: state.items
+//   };
+// };
 
-export default connect(mapStateToProps)(App);
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     addItem: () => {
+//       dispatch(addItem())
+//     }
+//   }
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+// this is the same as above just destructured
+
+// mapDispatchToProps allows us to bring in items, while mapStateToProps allows us to update state outside of the component
+
+export default connect(state => ({items: state.items}), {addItem})(App)
